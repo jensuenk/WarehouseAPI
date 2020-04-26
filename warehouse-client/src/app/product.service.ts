198b9f46e9210d297d8c98d081be8b3ccdd4928b
Jens Uenk <jens.uenk@gmail.com>
2020-04-27 01:49:19 +0200

    Added change/create/delete for products in the client

--- warehouse-client/src/app/product.service.ts
+++ warehouse-client/src/app/product.service.ts
@@ -1,19 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
+  private url = "https://192.168.0.158:45455/api/v1/product/";
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<IProduct> {
-      return this.http.get<IProduct>("https://192.168.0.158:45455/api/v1/product")
+      return this.http.get<IProduct>(this.url)
   }
   getProduct(id: number): Observable<IProduct> {
-    return this.http.get<IProduct>("https://192.168.0.158:45455/api/v1/product/id=" + id)
+    return this.http.get<IProduct>(this.url + "id=" + id)
+  }
+
+  createProduct(product: IProduct) {
+      let body = JSON.stringify(product);
+      return this.http.post(this.url, body, httpOptions);
+    }
+
+  updateProduct(product: IProduct) {
+      let body = JSON.stringify(product);
+      return this.http.put(this.url + product.id, body, httpOptions);
+  }
+
+  deleteProduct(product: IProduct) {
+      return this.http.delete(this.url + product.id);
   }
 
   product: IProduct;
--- warehouse-client/src/app/products/products.component.ts
+++ warehouse-client/src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct, ProductService } from '../product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -12,9 +13,69 @@ export class ProductsComponent implements OnInit {
   constructor(private svc : ProductService) { }
 
   ngOnInit() {
-    this.svc.getProducts()
-      .subscribe(result => this.products = result);
-    console.log(this.products)
+    this.getProducts()
   }
 
+  getProducts() {
+    this.svc.getProducts().subscribe(
+        result => {
+          this.products = result
+          return true;
+        },
+        error => {
+          console.error("Error while retreiving products!");
+          return Observable.throw(error);
+        }
+    );
+  }
+
+  createProduct(id, number, name, description, location, price) {
+    let newProduct: IProduct = {
+      id: id,
+      number: number,
+      name: name,
+      description: description,
+      location: location,
+      price: price,
+    }
+    this.svc.createProduct(newProduct).subscribe(
+        data => {
+          // refresh the list
+          this.getProducts();
+          return true;
+        },
+        error => {
+          console.error("Error creating product!");
+          return Observable.throw(error);
+        }
+    );
+  }
+    
+  updateProduct(product) {
+    this.svc.updateProduct(product).subscribe(
+        data => {
+          // refresh the list
+          this.getProducts();
+          return true;
+        },
+        error => {
+          console.error("Error saving product!");
+          return Observable.throw(error);
+        }
+    );
+  }
+    
+  deleteProduct(product) {
+    this.svc.deleteProduct(product).subscribe(
+        data => {
+          // refresh the list
+          this.getProducts();
+          return true;
+        },
+        error => {
+          console.error("Error deleting product!");
+          return Observable.throw(error);
+        }
+    );
+  }
 }

ace0d75bba6685f18100aa39dc408ddadedf5ff1
Jens Uenk <jens.uenk@gmail.com>
2020-05-07 13:09:36 +0200

    Changed default page length to 100 for all controllers

--- warehouse-client/src/app/app.module.ts
+++ warehouse-client/src/app/app.module.ts
@@ -2,26 +2,29 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductService } from './product.service';
-import {PanelModule} from 'primeng/panel';
-import {TableModule} from 'primeng/table';
-import {MessagesModule} from 'primeng/messages';
-import {MessageModule} from 'primeng/message';
-import {UsersComponent } from './users/users.component';
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
+
+import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { UsersComponent } from './users/users.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    UsersComponent
+    UsersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +37,14 @@ import {ButtonModule} from 'primeng/button';
     MessagesModule,
     MessageModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    RouterModule.forRoot([
+      {path: "users", component: UsersComponent},
+      {path: "orders", component: OrdersComponent},
+      {path: "products", component: ProductsComponent},
+      {path: "", redirectTo: "users", pathMatch: 'full'},
+      {path: "**", redirectTo: "users"},
+    ], {useHash: true})
   ],
   providers: [
     ProductService
